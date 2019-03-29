9685da38f0c4bd61ee429fc6a9de06a737c902f0
Andrea Johnson <andrea.johnson@vml.com>
2019-03-29 12:07:16 -0500

    Played around with map UI options settings, but I think they're out of date and I'm not going to keep them

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 // This is where I instantiate the map:
 function initialize() {
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 11
+    zoom: 11,
+    zoomControl: true,
+    zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.MEDIUM,
+        position: google.maps.ControlPosition.BOTTOM_LEFT
+    }
+    // disableDefaultUI: true,
+    // zoomControl: true,
+    // panControl: true,
+    // mapTypeControl: true,
+    // scaleControl: true,
+    // streetViewControl: true,
+    // rotateControl: true,
+    // overviewMapControl: true 
   });
 
   var latLong = new google.maps.LatLng(39.0997, -94.5786);
   map.setCenter(latLong);
 
-  // to set basemap type (could put this in a new function -- on certain button click, change map type ):
-  // map.setMapTypeId(google.maps.MapTypeId.HYBRID);
-
   changeTerrain(map);
 } // end function initialize
 
-// changeTerrain function receives the map object created in the initialize function
+// this changeTerrain function receives the map object created in the initialize function
 function changeTerrain(map) {
   // goal with this function: link buttons with event handlers and sets map type appropriately
   document.getElementById('btnTerrain').addEventListener("click", function() {
